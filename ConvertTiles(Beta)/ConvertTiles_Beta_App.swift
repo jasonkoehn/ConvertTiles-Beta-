469542e97acde403e3f7b1b61a0b2e938f831bd0
jasonkoehn <93397575+jasonkoehn@users.noreply.github.com>
2022-12-12 14:09:18 -0700

    Initial Commit

---
+++ ConvertTiles(Beta)/ConvertTiles_Beta_App.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertTiles_Beta_App.swift
+//  ConvertTiles(Beta)
+//
+//  Created by Jason Koehn on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ConvertTiles_Beta_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
